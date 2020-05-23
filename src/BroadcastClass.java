0fb428649268e9a6b3b0efe5aa1f461c780c8ddd
Filip Jurec <filip.jurec@triple-innovations.com>
2020-05-23 09:18:08 +0200

    First methods added.

--- src/BroadcastClass.java
+++ src/BroadcastClass.java
@@ -1,10 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class BroadcastClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-
+	public static void main(String[] args) throws IOException {
+	 	ProcessBuilder builder = new ProcessBuilder(
+		            "cmd.exe", "/c", "cd \"C:\\Users\\Korisnik\\Desktop\\ffmpeg-20200522-38490cb-win64-static\\bin>\" && ffmpeg -re -f lavfi -i aevalsrc=\"sin(400*2*PI*t)\" -ar 8000 -f mulaw -f rtp rtp://127.0.0.1:1234");
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) { break; }
+            System.out.println(line);
+		}
 	}
+	
+	public static void connectToServer() {
+        //Try connect to the server on an unused port eg 9991. A successful connection will return a socket
+        try(ServerSocket serverSocket = new ServerSocket(9991)) {
+            Socket connectionSocket = serverSocket.accept();
 
+            //Create Input&Outputstreams for the connection
+            InputStream inputToServer = connectionSocket.getInputStream();
+            OutputStream outputFromServer = connectionSocket.getOutputStream();
+
+            Scanner scanner = new Scanner(inputToServer, "UTF-8");
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+
+            serverPrintOut.println("Hello World! Enter Peace to exit.");
+
+            //Have the server take input from the client and echo it back
+            //This should be placed in a loop that listens for a terminator text e.g. bye
+            boolean done = false;
+
+            while(!done && scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                serverPrintOut.println("Echo from <Your Name Here> Server: " + line);
+
+                if(line.toLowerCase().trim().equals("peace")) {
+                    done = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
